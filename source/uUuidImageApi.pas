4566d976ce46773d7aa7165482ac2494f6dcd65e
TheOriginalBytePlayer <prez@frameforge3d.com>
2026-08-20 15:07:04 -0700

    Add uUuidImageApi unit for UUID image management

---
+++ source/uUuidImageApi.pas
@@ -0,0 +1,72 @@
+unit uUuidImageApi;
+
+interface
+
+uses
+  System.SysUtils, System.Classes,
+  uImgResourceHandler;
+
+procedure RegisterUuidImage(const AGuid: TGUID; const ABytes: TBytes); overload;
+procedure RegisterUuidImage(const AGuidText: string; const ABytes: TBytes); overload;
+function RegisterUuidImageFromFile(const AGuid: TGUID; const AFileName: string): Boolean; overload;
+function RegisterUuidImageFromFile(const AGuidText: string; const AFileName: string): Boolean; overload;
+procedure UnregisterUuidImage(const AGuid: TGUID); overload;
+procedure UnregisterUuidImage(const AGuidText: string); overload;
+procedure ClearUuidImages;
+
+implementation
+
+procedure RegisterUuidImage(const AGuid: TGUID; const ABytes: TBytes);
+begin
+  TUuidImageStore.Put(AGuid, ABytes);
+end;
+
+procedure RegisterUuidImage(const AGuidText: string; const ABytes: TBytes);
+begin
+  TUuidImageStore.Put(AGuidText, ABytes);
+end;
+
+function RegisterUuidImageFromFile(const AGuid: TGUID; const AFileName: string): Boolean;
+var
+  FS: TFileStream;
+  B: TBytes;
+begin
+  Result := False;
+  if not FileExists(AFileName) then Exit;
+
+  FS := TFileStream.Create(AFileName, fmOpenRead or fmShareDenyNone);
+  try
+    SetLength(B, FS.Size);
+    if FS.Size > 0 then
+      FS.ReadBuffer(B[0], FS.Size);
+  finally
+    FS.Free;
+  end;
+
+  TUuidImageStore.Put(AGuid, B);
+  Result := True;
+end;
+
+function RegisterUuidImageFromFile(const AGuidText: string; const AFileName: string): Boolean;
+var
+  G: TGUID;
+begin
+  Result := TryStrToGUID(Trim(AGuidText), G) and RegisterUuidImageFromFile(G, AFileName);
+end;
+
+procedure UnregisterUuidImage(const AGuid: TGUID);
+begin
+  TUuidImageStore.Remove(AGuid);
+end;
+
+procedure UnregisterUuidImage(const AGuidText: string);
+begin
+  TUuidImageStore.Remove(AGuidText);
+end;
+
+procedure ClearUuidImages;
+begin
+  TUuidImageStore.Clear;
+end;
+
+end.
