cbb004e29f4fe20f7632a80df41191db10742c8f
TheOriginalBytePlayer <prez@frameforge3d.com>
2026-08-20 15:20:15 -0700

    Implement UUID scheme factory for image resources
    Added UUID scheme factory registration for image handling.

--- source/uUuidImageApi.pas
+++ source/uUuidImageApi.pas
@@ -3,8 +3,7 @@ unit uUuidImageApi;
 interface
 
 uses
-  System.SysUtils, System.Classes,
-  uImgResourceHandler;
+  System.SysUtils, System.Classes;
 
 procedure RegisterUuidImage(const AGuid: TGUID; const ABytes: TBytes); overload;
 procedure RegisterUuidImage(const AGuidText: string; const ABytes: TBytes); overload;
@@ -13,16 +12,47 @@ function RegisterUuidImageFromFile(const AGuidText: string; const AFileName: str
 procedure UnregisterUuidImage(const AGuid: TGUID); overload;
 procedure UnregisterUuidImage(const AGuidText: string); overload;
 procedure ClearUuidImages;
+procedure RegisterUuidSchemeFactory;
 
 implementation
 
+uses
+  uCEFApplication, uCEFInterfaces, uCEFSchemeHandlerFactory, uCEFBrowser, uCEFRequest,
+  uImgResourceHandler;
+
+var 
+  RegisteredFactory;
+
+type
+  TUuidSchemeHandlerFactory = class(TCefSchemeHandlerFactoryOwn)
+  protected
+    function New(const browser: ICefBrowser; const frame: ICefFrame;
+      const schemeName: ustring; const request: ICefRequest): ICefResourceHandler; override;
+  end;
+
+procedure RegisterUuidSchemeFactory;
+begin
+  RegisteredFactory:=CefRegisterSchemeHandlerFactory('uuid', '', TUuidSchemeHandlerFactory.Create);
+end;
+
+function TUuidSchemeHandlerFactory.New(const browser: ICefBrowser;
+  const frame: ICefFrame; const schemeName: ustring;
+  const request: ICefRequest): ICefResourceHandler;
+begin
+  Result := TImgResourceHandler.Create;
+end;
+
 procedure RegisterUuidImage(const AGuid: TGUID; const ABytes: TBytes);
 begin
+  if not RegisteredFactory then 
+    RegisterUuidSchemeFactory;
   TUuidImageStore.Put(AGuid, ABytes);
 end;
 
 procedure RegisterUuidImage(const AGuidText: string; const ABytes: TBytes);
 begin
+  if not RegisteredFactory then 
+    RegisterUuidSchemeFactory;
   TUuidImageStore.Put(AGuidText, ABytes);
 end;
 
@@ -31,6 +61,8 @@ var
   FS: TFileStream;
   B: TBytes;
 begin
+  if not RegisteredFactory then 
+    RegisterUuidSchemeFactory;
   Result := False;
   if not FileExists(AFileName) then Exit;
 
@@ -51,22 +83,39 @@ function RegisterUuidImageFromFile(const AGuidText: string; const AFileName: str
 var
   G: TGUID;
 begin
+  if not RegisteredFactory then 
+    RegisterUuidSchemeFactory;
   Result := TryStrToGUID(Trim(AGuidText), G) and RegisterUuidImageFromFile(G, AFileName);
 end;
 
 procedure UnregisterUuidImage(const AGuid: TGUID);
 begin
+  if not RegisteredFactory then 
+    exit;
   TUuidImageStore.Remove(AGuid);
 end;
 
 procedure UnregisterUuidImage(const AGuidText: string);
 begin
+  if not RegisteredFactory then 
+    exit;
   TUuidImageStore.Remove(AGuidText);
 end;
 
 procedure ClearUuidImages;
 begin
+  if not RegisteredFactory then 
+    exit;
+  
   TUuidImageStore.Clear;
 end;
+
+initialization
+  RegisteredFactory:=False;
+
+finalization
+
+ if RegisteredFactory then 
+   CefClearSchemeHandlerFactories();
 
 end.
